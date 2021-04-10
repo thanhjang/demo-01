a331f8286bc5e7e68746d57c74535654ab129d5f
DESKTOP-TM7HO9R\Nhung Nguyễn <thanhtiny2k@gmail.com>
2021-04-10 18:20:45 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+export default class App extends Component {
+  render() {
+    return (
+        <View>
+          <Text>Hello World ! </Text>
+        </View>
+    );
+  }
+}
